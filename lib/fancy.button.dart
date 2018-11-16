8dc53ebe255577d0ac65a42398319a8b2cd1c0a3
tomguo <huabing.guo@blockheaders.com>
2018-11-16 16:13:17 +0800

    first commit

---
+++ lib/fancy.button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class FancyButton extends StatelessWidget {
+  FancyButton({@required this.onPressed});
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return
+      RawMaterialButton(
+      fillColor: Colors.deepOrange,
+      splashColor: Colors.orange,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: const <Widget>[
+            RotatedBox(
+            quarterTurns:3,
+            child:Icon(Icons.explore,
+              color: Colors.amber,
+            )),
+            SizedBox(width: 8.0),
+            Text(
+              "查看我喜欢的收藏",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        )
+      ),
+      onPressed: onPressed,
+      shape: const StadiumBorder(),
+    );
+  }
+}
